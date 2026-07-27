9a8aea84a64fbd085d51632bda67f7e7c8933ac4
Mohamed Javeed M <mmohamedjaveed30@gmail.com>
2026-07-27 10:56:43 +0530

    Simplify login.js by removing unnecessary code
    Refactor login script to streamline login process and remove redundant code.

--- playwright/login.js
+++ playwright/login.js
@@ -1,87 +1,33 @@
-const { chromium } = require("playwright");
+const { chromium } = require('playwright');
 
+(async () => {
 
-(async()=>{
+    const browser = await chromium.launch({
 
+        headless: true,
 
-const browser = await chromium.launch({
-    headless:true
-});
+        proxy: {
+            server: 'http://localhost:8080'
+        }
 
+    });
 
-const context = await browser.newContext();
+    const page = await browser.newPage();
 
+    await page.goto("http://localhost:3000");
 
-const page = await context.newPage();
+    await page.locator("#navbarAccount").click();
 
+    await page.locator("#navbarLoginButton").click();
 
+    await page.locator("#email").fill(process.env.APP_USERNAME);
 
-console.log("Opening Juice Shop");
+    await page.locator("#password").fill(process.env.APP_PASSWORD);
 
+    await page.locator("#loginButton").click();
 
-await page.goto(
-    "http://localhost:3000"
-);
+    await page.waitForTimeout(5000);
 
-
-await page.waitForTimeout(3000);
-
-
-
-console.log("Opening Login");
-
-
-await page.goto(
-    "http://localhost:3000/#/login"
-);
-
-
-
-await page.waitForTimeout(3000);
-
-
-
-await page.fill(
-    "#email",
-    process.env.APP_USERNAME
-);
-
-
-
-await page.fill(
-    "#password",
-    process.env.APP_PASSWORD
-);
-
-
-
-await page.click(
-    "#loginButton"
-);
-
-
-
-await page.waitForTimeout(5000);
-
-
-
-console.log("Login Completed");
-
-
-
-// Save authentication session
-
-await context.storageState({
-
-    path:"auth.json"
-
-});
-
-
-console.log("Auth state saved");
-
-
-await browser.close();
-
+    await browser.close();
 
 })();
